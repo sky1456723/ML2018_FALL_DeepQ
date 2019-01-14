aea971f97c95280746a56561b8ba5324cf8aaeae
sky1456723 <b05901009@ntu.edu.tw>
2019-01-14 11:51:07 +0000

    Update shell script

--- src/finalscript.sh
+++ src/finalscript.sh
@@ -1,7 +1,7 @@
-python3 ./AE.py --root_dir=$1 --img_dirs=$2  --model_name=AutoEncoder 
+python3 ./AE.py --root_dir $1 --imgs_dir $2 -n --model_name AutoEncoder 
 for i in {1..13}
 do
-	python3 ./combine.py -u=AutoEncoder --model_name=model_$i -r=$1 -i=$2
+	python3 ./combine.py -u AutoEncoder --model_name model_$i -r $1 -i $2
 done
 for j in {1..13}
 do
