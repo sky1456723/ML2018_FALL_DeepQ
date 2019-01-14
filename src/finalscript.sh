d9b61fae67beb1a588e8666f709128de546a92ba
Serenade34 <43457610+Serenade34@users.noreply.github.com>
2019-01-14 16:47:35 +0800

    Add files via upload

--- src/finalscript.sh
+++ src/finalscript.sh
@@ -1,10 +1,10 @@
-python3 AE.py --root_dir=$1 --img_dirs=$2  --model_name=AutoEncoder 
+python3 ./AE.py --root_dir=$1 --img_dirs=$2  --model_name=AutoEncoder 
 for i in {1..12}
 do
-	python3 combine.py -u=AutoEncoder --model_name=model_$i -r=$1 -i=$2
+	python3 ./combine.py -u=AutoEncoder --model_name=model_$i -r=$1 -i=$2
 done
 for j in {1..12}
 do
-	python3 test_combine_model.py model_$j model_$j.csv $1 $2
+	python3 ./test_combine_model.py ./model_$j ./model_$j.csv $1 $2
 done
-python3 ensemble.py
+python3 ./ensemble.py
